624d2e41e97c82a7c5528ca45704cc665c8adc99
bysophiamarie <32103427+bysophiamarie@users.noreply.github.com>
2017-12-13 15:58:25 -0500

    commit

--- chloropleth_complete/index.js
+++ chloropleth_complete/index.js
@@ -9,22 +9,24 @@ var svg = d3.select('svg')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
 //set up the projection for the map
-var albersProjection = d3.geoAlbersUsa()  //tell it which projection to use
+var mercatorProjection = d3.geoMercator()  //tell it which projection to use
     .scale(700)                           //tell it how big the map should be
     .translate([(width/2), (height/2)]);  //set the center of the map to show up in the center of the screen
 
 //set up the path generator function to draw the map outlines
 path = d3.geoPath()
-    .projection(albersProjection);        //tell it to use the projection that we just made to convert lat/long to pixels
+    .projection(mercatorProjection);        //tell it to use the projection that we just made to convert lat/long to pixels
 
 var stateLookup = d3.map();
 
 var colorScale = d3.scaleLinear().range(['white','blue']);
 
 queue()
-    .defer(d3.json, "./cb_2016_us_state_20m.json")
-    .defer(d3.csv, "./statePop.csv")
-    .await(function(err, mapData, populationData){
+    .defer(d3.json, "./locations.json")
+    .defer(d3.json, "./regions.json")
+    .defer(d3.json, "./mountains_lakes.json")
+    //.defer(d3.csv, "./statePop.csv")
+    .await(function(err, locations, regions, mountains_lakes){
 
 
     populationData.forEach(function(d){
@@ -34,19 +36,28 @@ queue()
 
     colorScale.domain([0, d3.max(populationData.map(function(d){return +d.population}))]);
 
-    svg.selectAll("path")               //make empty selection
-        .data(mapData.features)          //bind to the features array in the map data
+    svg.selectAll("path.locations")               //make empty selection
+        .data(locations.features)          //bind to the features array in the map data
         .enter()
         .append("path")                 //add the paths to the DOM
         .attr("d", path)                //actually draw them
-        .attr("class", "feature")
+        .attr("class", "locations")
         .attr('fill',function(d){
             return colorScale(stateLookup.get(d.properties.NAME));
         })
         .attr('stroke','white')
         .attr('stroke-width',.2);
 
+        svg.selectAll("path.regions")               //make empty selection
+            .data(regions.features)          //bind to the features array in the map data
+            .enter()
+            .append("path")                 //add the paths to the DOM
+            .attr("d", path)                //actually draw them
+            .attr("class", "regions")
+            .attr('fill',function(d){
+                return colorScale(stateLookup.get(d.properties.NAME));
+            })
+            .attr('stroke','white')
+            .attr('stroke-width',.2);
+
   });
-
-
-
